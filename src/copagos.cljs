4f403eda5ba9102bdc5c03dbec6e0afdb3fec101
José Javier Blanco Rivero <javierweiss@gmail.com>
2024-10-18 15:37:50 -0300

    visual de tabla de planes actuales

--- src/copagos.cljs
+++ src/copagos.cljs
@@ -6,7 +6,7 @@
 
 (def datos (r/atom (hash-map)))
 
-(def obra (atom 0))
+(def obra (r/atom 0))
 
 (defn limpiar-checkboxes
   []
@@ -73,9 +73,18 @@
     :else (conj coll valor_nuevo)))
 
 (defn buscar-planes-actuales
-  []
-  (js/fetch (str "/planes?obra=" @obra) (clj->js {:method "GET"})))
-
+  [atom-coll] 
+  (-> (js/fetch (str "/planes?obra=" #_@obra 1900))
+      (p/then (fn [response]
+                (if (.-ok response)
+                  (-> (.json response)
+                      (p/then (fn [cuerpo]
+                                (prn (js->clj cuerpo))
+                                (reset! atom-coll (js->clj cuerpo :keywordize-keys true)))))
+                  (js/console.error "No existen registros" response))))
+      (p/catch (fn [error]
+                 (js/console.error "Error en la solicitud" error)))))
+ 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;; COMPONENTES ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn formulario
@@ -134,10 +143,24 @@
      [:button {:on-click enviar} "Enviar" ]
      [:button {:on-click limpiar} "Cancelar" ]]]])
 
-(defn historia
+(defn tabla-planes-actuales
   []
-  [:div#historia {:hidden true}
-   [:h3 "Registro histórico"]])
+  (let [resultado (r/atom [])
+        _ (buscar-planes-actuales resultado)] 
+    (fn [] 
+      [:div.tabla
+       [:table
+        [:tr 
+         [:th "Obra/plan"]
+         [:th "Especialidad"]
+         [:th "Categoría"]
+         [:th "Copago"]]
+        (for [elem @resultado]
+          [:tr 
+           [:td (:tbl_planes_obras_sociales/codplan elem)]
+           [:td (:tbl_planes_obras_sociales/especialidad elem)]
+           [:td (:tbl_planes_obras_sociales/categoria elem)]
+           [:td (str (:tbl_planes_obras_sociales/copago elem) " $")]])]])))
 
 (defn visual-registros
   []
@@ -148,20 +171,23 @@
             :value @obra
             :placeholder "Ingrese el código de obra social"
             :on-change #(reset! obra (->> % .-target .-value (.parseFloat js/Number)))}]
-   [:button {:on-click buscar-planes-actuales} "Buscar"]])
-
-(defn tabla-planes-actuales
-  [datos]
-  [:div.tabla])
+   (when (not= 0 @obra)
+     [tabla-planes-actuales])])
 
 (defn tabla-historico
-  [datos]
+  []
   [:div.tabla])
+
+(defn historia
+  []
+  [:div#historia {:hidden true}
+   [:h3 "Registro histórico"]])
 
 (defn pagina
   []
   [:<> 
-   [formulario]
+   [tabla-planes-actuales]
+   #_#_#_[formulario]
    [historia]
    [visual-registros]])
 
@@ -169,10 +195,10 @@
 
 
 (comment
-  
+
   (limpiar-fecha)
   (limpiar-checkboxes)
-   
+
   (.stringify js/JSON (clj->js {:a 33 :b 334}))
 
   (-> (js/fetch "/guardar" {:method "POST" :body (.stringify js/JSON
@@ -183,42 +209,59 @@
       (p/catch #(js/console.error "Upps!!")))
 
   (p/-> (js/fetch "/guardar" (clj->js {:headers {"Content-Type" "application/json"}
-                                     :method "POST"
-                                     :body (->> {:codplan "108-A"
-                                                 :especialidad 931
-                                                 :copago 1500}
-                                                clj->js
-                                                (.stringify js/JSON))}))
-        ((juxt #(.-ok %) #(.json %))) 
+                                       :method "POST"
+                                       :body (->> {:codplan "108-A"
+                                                   :especialidad 931
+                                                   :copago 1500}
+                                                  clj->js
+                                                  (.stringify js/JSON))}))
+        ((juxt #(.-ok %) #(.json %)))
         ((fn [[ok prm]]
            (if ok
              (js/alert "Todo bien!!")
-              prm)))
-        (some-> 
-         .-error 
+             prm)))
+        (some->
+         .-error
          (string/includes? "duplicate key")
-         (if 
-            (js/alert "Ya existe un registro con esos datos")
-            (js/alert "Hubo un error"))))
-      
-   
+         (if
+          (js/alert "Ya existe un registro con esos datos")
+           (js/alert "Hubo un error"))))
+
+
   (.confirm js/window "¿Está seguro?") (.parseFloat js/Number "23.55")
 
   (-> {:a 1 :z #{:a :b}} :z)
-  
+
   (let [p "p,f,e,r,t"
         pl "K, L, Ñ, E"
         pla " D ,A ,e , a"]
-    (parsear-planes 1820 pla)) 
-           
-  @datos   
+    (parsear-planes 1820 pla))
+
+  @datos
   (every? @datos [:obra :plan :especialidad :copago :vigencia])
 
   (validar-datos @datos)
-
-  (-> (buscar-planes-actuales) (p/then #(js/console.log %)))
   
-  (js/React.useEffect )
+  (reset! obra 1900)
+ 
+  @obra
+  
+  (let [c (r/atom [])]
+    (buscar-planes-actuales c)
+      @c)
+  
+  (let [atom-coll (r/atom [])]
+    (-> (js/fetch (str "/planes?obra=" @obra))
+        (p/then #(when (.-ok %)
+                   (p/let [cuerpo (.json %)]
+                     (p/then (fn [data]
+                               (js/console.log "JSON data:" data)
+                               (reset! atom-coll (js->clj data))))))))
+    @atom-coll) 
+ 
+  (buscar-planes-actuales)
+   
+  (js/React.useEffect)
   )  
   
  
